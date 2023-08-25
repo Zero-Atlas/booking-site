d7d2cc61fa78d1ea19b8a080dc0b1f255f9f538a
Huỳnh Đắc An <118910962+Zero-Atlas@users.noreply.github.com>
2023-08-25 13:05:45 +0000

    f

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ const searchRouter = require("./router/search");
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@movie-site.kpkcv1h.mongodb.net/${process.env.MONGO_DATABASE}`;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.options(cors({
   origin: [
     "https://booking-site-admin.vercel.app",
@@ -21,9 +24,10 @@ app.options(cors({
   allowedHeaders: "*",
 }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/home", homeRouter);

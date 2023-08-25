8fcfdcad22da409a2eea1f2e8dd4f99d7437ca4d
Huỳnh Đắc An <118910962+Zero-Atlas@users.noreply.github.com>
2023-08-25 08:43:47 +0000

    allow all origin

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 //   })
 // );
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://booking-site-admin.vercel.app/, https://booking-site-silk.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
